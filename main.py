fdf7034160c93fa30d1dd75759eecee0a35a4174
Latifa-Git-dev <latifa.omary@gmx.de>
2020-06-19 15:58:20 +0200

    In the main file: - created GameDisplay class     -- implemented displaymessageaccordingtostate function - created an instance of the class - called the displaymessageaccordingtostate function

---
+++ main.py
@@ -0,0 +1,8 @@
+class GameDisplay:
+    def displaymessageaccordingtostate(state):
+        if state == 0:
+            print("Welcome to the game!")
+
+
+gamedisplay = GameDisplay
+gamedisplay.displaymessageaccordingtostate(0)
